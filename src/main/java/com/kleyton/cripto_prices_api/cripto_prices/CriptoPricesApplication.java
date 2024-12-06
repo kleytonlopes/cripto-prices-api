1944695448c327fc35fbdf9a205fc16bbe3d396a
Kleyton Lopes <kleyton.lop@gmail.com>
2024-12-05 21:36:06 -0300

    Commit inicial

---
+++ src/main/java/com/kleyton/cripto_prices_api/cripto_prices/CriptoPricesApplication.java
@@ -0,0 +1,13 @@
+package com.kleyton.cripto_prices_api.cripto_prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleyton/cripto_prices_api/cripto_prices/CriptoPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleyton.cripto_prices_api.cripto_prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
